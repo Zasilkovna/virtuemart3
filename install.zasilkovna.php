2af11d5045fe65e73f385fae29b266efd42df6a5
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2021-04-16 09:04:28 +0200

    PES-76: CR delete dir

--- install.zasilkovna.php
+++ install.zasilkovna.php
@@ -183,27 +183,6 @@ INSERT INTO #__virtuemart_adminmenuentries (`module_id`, `parent_id`, `name`, `l
 		}
 	}
 
-    /**
-     * @param $dirPath
-     */
-    private static function deleteDir($dirPath) {
-        if (!is_dir($dirPath)) {
-            throw new InvalidArgumentException("$dirPath must be a directory");
-        }
-        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-            $dirPath .= '/';
-        }
-        $files = glob($dirPath . '*', GLOB_MARK);
-        foreach ($files as $file) {
-            if (is_dir($file)) {
-                self::deleteDir($file);
-            } else {
-                unlink($file);
-            }
-        }
-        rmdir($dirPath);
-    }
-
 	/**
 	 * Called on installation
 	 *
