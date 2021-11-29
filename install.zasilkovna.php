4fc71a54866bb7e2ccf1eb340e65a0107d4f61ca
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2021-11-29 12:31:46 +0100

    PES-169: uninstall update

--- install.zasilkovna.php
+++ install.zasilkovna.php
@@ -465,6 +465,7 @@ INSERT INTO #__virtuemart_adminmenuentries (`module_id`, `parent_id`, `name`, `l
         recurse_delete($vm_admin_path . DS . 'models' . DS . 'zasilkovna_src' . DS);
         recurse_delete($vm_admin_path . DS . 'views' . DS . 'zasilkovna' . DS);
         recurse_delete($vm_admin_path . DS . 'controllers' . DS . 'zasilkovna.php');
+        recurse_delete($vm_admin_path . DS . 'fields' . DS . 'vmzasilkovnacountries.php');
         recurse_delete(JPATH_ADMINISTRATOR . DS . 'language' . DS . 'en-GB' . DS . 'en-GB.plg_vmshipment_zasilkovna.ini');
         recurse_delete(JPATH_ADMINISTRATOR . DS . 'language' . DS . 'cs-CZ' . DS . 'cs-CZ.plg_vmshipment_zasilkovna.ini');
         recurse_delete(JPATH_ADMINISTRATOR . DS . 'language' . DS . 'sk-SK' . DS . 'sk-SK.plg_vmshipment_zasilkovna.ini', true);
