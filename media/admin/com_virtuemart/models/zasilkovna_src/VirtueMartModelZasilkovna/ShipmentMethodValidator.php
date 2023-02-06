acdeadf59559e6095111a6bd1bac026b3ea9523f
Oldrich Korec Mraz <oldrich.mraz@packeta.com>
2023-02-06 08:44:49 +0100

    PES-1206 CR Fix 2
    refactor ShipmentMethodValidator

--- media/admin/com_virtuemart/models/zasilkovna_src/VirtueMartModelZasilkovna/ShipmentMethodValidator.php
+++ media/admin/com_virtuemart/models/zasilkovna_src/VirtueMartModelZasilkovna/ShipmentMethodValidator.php
@@ -7,121 +7,6 @@ use VirtueMartModelCountry;
 class ShipmentMethodValidator
 {
     /**
-     * @return ShipmentValidationReport
-     */
-
-    public function validate(ShipmentMethod $shipmentMethod)
-    {
-        $report = new \VirtueMartModelZasilkovna\ShipmentValidationReport();
-
-        $globalDefaultPrice = $shipmentMethod->getGlobalDefaultPrice();
-        if (empty($globalDefaultPrice) && !is_numeric($globalDefaultPrice)) {
-            $report->addError(ShipmentValidationReport::ERROR_CODE_GLOBAL_DEFAULT_PRICE_MISSING);
-        } else {
-            if (!is_numeric($globalDefaultPrice)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
-            }
-        }
-
-        $globalMaxWeight = $shipmentMethod->getGlobalMaxWeight();
-        if (empty($globalMaxWeight) && !is_numeric($globalMaxWeight)) {
-            $report->addError(ShipmentValidationReport::ERROR_CODE_GLOBAL_MAX_WEIGHT_MISSING);
-        } else {
-            if (!is_numeric($globalMaxWeight)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
-            }
-        }
-
-        $weightsFE = ($shipmentMethod->getGlobalWeightRules() ?: []);
-        foreach ($weightsFE as $weightRule) {
-            $weightRulesReport = $this->validateWeightRule($weightRule, $globalMaxWeight);
-
-            if ($weightRulesReport->isValid() === false) {
-                $report->merge($weightRulesReport);
-                break;
-            }
-        }
-
-        $rules = ($shipmentMethod->getPricingRules() ?: []);
-        $countries = [];
-
-        foreach ($rules as $countryRule) {
-            if (array_key_exists($countryRule->country, $countries)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_DUPLICATE_COUNTRIES); // multiple country definitions not allowed
-                break;
-            }
-
-            $countries[$countryRule->country] = $countryRule->country;
-
-            $countryWeightRules = ($shipmentMethod->getCountryWeightRules($countryRule->country) ?: []);
-            foreach ($countryWeightRules as $weightRule) {
-                $weightRulesReport = $this->validateWeightRule($weightRule, $globalMaxWeight);
-
-                if ($weightRulesReport->isValid() === false) {
-                    $report->merge($weightRulesReport);
-                    break;
-                }
-            }
-        }
-
-        $blockingCountries = $shipmentMethod->getBlockingCountries();
-        $allowedCountries = $shipmentMethod->getAllowedCountries();
-
-        $blockingCountries = array_diff($blockingCountries, $allowedCountries); // when user allowes and blocks same countries
-        $allowedCountries = array_diff($allowedCountries, $blockingCountries); // when user allowes and blocks same countries
-
-        if (!empty($allowedCountries)) {
-            $diff = array_diff($countries, $allowedCountries);
-            if (!empty($diff)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_ALLOWED_COUNTRIES_ONLY);
-            }
-        }
-
-        if (!empty($blockingCountries)) {
-            $diff = array_diff($countries, $blockingCountries);
-            if (count($diff) !== count($countries)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_NO_BLOCKED_COUNTRY);
-            }
-        }
-
-        $shippingType = $shipmentMethod->getShippingType();
-        $hdCarrierId = $shipmentMethod->getHdCarrierId();
-
-        if ($shippingType === 'pickuppoints' && $hdCarrierId !== null) {
-            $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_REDUNDANT_FOR_PP);
-        }
-
-        if ($shippingType === 'hdcarriers') {
-            if (empty($hdCarrierId)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_NO_HD_CARRIER_SELECTED);
-            } else {
-                $carrierData = $shipmentMethod->getCarrierRepository()->getCarrierData($hdCarrierId);
-                if ($carrierData === null || $carrierData->deleted === 1) {
-                    $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_NOT_EXISTS);
-
-                    return $report;
-                }
-                $vmCarrierCountry = VirtueMartModelCountry::getCountryByCode(strtoupper($carrierData->country));
-
-                if (!$vmCarrierCountry->published) {
-                    $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_NO_PUBLISHED_COUNTRY);
-                }
-                $carrierVmCountryId = $vmCarrierCountry->virtuemart_country_id;
-                if (!empty($allowedCountries) && !in_array($carrierVmCountryId, $allowedCountries, true)) {
-                    $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_IS_OUT_OF_ALLOWED_COUNTRIES);
-                }
-                if ((empty($allowedCountries) || in_array($carrierVmCountryId, $allowedCountries,
-                            true)) && in_array($carrierVmCountryId, $blockingCountries, true)) {
-                    $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_IS_IN_BLOCKING_COUNTRIES);
-                }
-            }
-
-        }
-
-        return $report;
-    }
-
-    /**
      * @param $weightRule
      * @param $maxWeight
      * @return \VirtueMartModelZasilkovna\ShipmentValidationReport
@@ -132,10 +17,8 @@ class ShipmentMethodValidator
 
         if (empty($weightRule->price) && !is_numeric($weightRule->price)) {
             $weightRulesReport->addError(ShipmentValidationReport::ERROR_CODE_WEIGHT_PRICE_MISSING);
-        } else {
-            if (!is_numeric($weightRule->price)) {
-                $weightRulesReport->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
-            }
+        } elseif (!is_numeric($weightRule->price)) {
+            $weightRulesReport->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
         }
 
         if (empty($weightRule->maxWeightKg) && !is_numeric($weightRule->maxWeightKg)) {
@@ -153,32 +36,39 @@ class ShipmentMethodValidator
         return $weightRulesReport;
     }
 
-    // split method validate to more methods to make it more readable
-    // split method validate to more methods to make it more readable
+    /**
+     * @param $shipmentMethod
+     * @return ShipmentValidationReport
+     */
     public function validate($shipmentMethod)
     {
         $report = new ShipmentValidationReport();
         $globalMaxWeight = $shipmentMethod->getGlobalMaxWeightKg();
         $this->validateGlobalWeightRules($report, $shipmentMethod, $globalMaxWeight);
 
-        $this->validateCountryWeightRulesAndCountries($report, $shipmentMethod, $globalMaxWeight
-
-        $this->validateCountries($report, $shipmentMethod);
+        $this->validateCountryWeightRulesAndCountries($report, $shipmentMethod, $globalMaxWeight);
 
         $this->validateHdCarrier($report, $shipmentMethod);
 
         return $report;
     }
 
-    private function validateGlobalWeightRules(ShipmentValidationReport $report, ShipmentMethod $shipmentMethod, $globalMaxWeight
-    {
+    /**
+     * @param ShipmentValidationReport $report
+     * @param ShipmentMethod $shipmentMethod
+     * @param $globalMaxWeight
+     * @return void
+     */
+    private function validateGlobalWeightRules(
+        ShipmentValidationReport $report,
+        ShipmentMethod $shipmentMethod,
+        $globalMaxWeight
+    ) {
 
         if (empty($globalMaxWeight) && !is_numeric($globalMaxWeight)) {
             $report->addError(ShipmentValidationReport::ERROR_CODE_GLOBAL_MAX_WEIGHT_MISSING);
-        } else {
-            if (!is_numeric($globalMaxWeight)) {
-                $report->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
-            }
+        } elseif (!is_numeric($globalMaxWeight)) {
+            $report->addError(ShipmentValidationReport::ERROR_CODE_INVALID_TYPE);
         }
 
         $weightsFE = ($shipmentMethod->getGlobalWeightRules() ?: []);
@@ -192,8 +82,17 @@ class ShipmentMethodValidator
         }
     }
 
-    private function validateCountryWeightRulesAndCountries(ShipmentValidationReport $report, ShipmentMethod $shipmentMethod, $globalMaxWeight)
-    {
+    /**
+     * @param ShipmentValidationReport $report
+     * @param ShipmentMethod $shipmentMethod
+     * @param $globalMaxWeight
+     * @return void
+     */
+    private function validateCountryWeightRulesAndCountries(
+        ShipmentValidationReport $report,
+        ShipmentMethod $shipmentMethod,
+        $globalMaxWeight
+    ) {
         $rules = ($shipmentMethod->getPricingRules() ?: []);
         $countries = [];
 
@@ -240,14 +139,16 @@ class ShipmentMethodValidator
     }
 
     /**
-     * @param $report
-     * @param $shipmentMethod
-     * @return mixed|void
+     * @param ShipmentValidationReport $report
+     * @param ShipmentMethod $shipmentMethod
+     * @return void
      */
-    public function validateHdCarrier($report, $shipmentMethod)
+    public function validateHdCarrier(ShipmentValidationReport $report, ShipmentMethod $shipmentMethod)
     {
         $shippingType = $shipmentMethod->getShippingType();
         $hdCarrierId = $shipmentMethod->getHdCarrierId();
+        $blockingCountries = $shipmentMethod->getBlockingCountries();
+        $allowedCountries = $shipmentMethod->getAllowedCountries();
 
         if ($shippingType === 'pickuppoints' && $hdCarrierId !== null) {
             $report->addError(ShipmentValidationReport::ERROR_CODE_HD_CARRIER_REDUNDANT_FOR_PP);
