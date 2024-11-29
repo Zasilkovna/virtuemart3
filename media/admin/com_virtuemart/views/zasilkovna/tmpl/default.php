8eb710813e282a83b19001ff591ced0cf33dcc13
David Belka <david.belka@yahoo.co.uk>
2024-11-29 16:39:58 +0100

    Revert "PES-2400: Resolved an issue where the VM side menu would render with inconsistent styling in Joomla 3.10.x and Virtuemart 3.8.8"
    This reverts commit 2b2e2afbbc34c6929f0ef64a44bb314d331085b4.

--- media/admin/com_virtuemart/views/zasilkovna/tmpl/default.php
+++ media/admin/com_virtuemart/views/zasilkovna/tmpl/default.php
@@ -5,8 +5,7 @@
 
     $adminTemplate = VMPATH_ROOT . '/administrator/templates/vmadmin/html/com_virtuemart/';
     JLoader::register('vmuikitAdminUIHelper', $adminTemplate . 'helpers/vmuikit_adminuihelper.php');
-    $isBelowJ4AndVM4 = version_compare(JVERSION, '4.0.0', '<') && version_compare(VmConfig::getInstalledVersion(), '4.0.0', '<');
-    if (class_exists('vmuikitAdminUIHelper') && !$isBelowJ4AndVM4) {
+    if (class_exists('vmuikitAdminUIHelper')) {
         vmuikitAdminUIHelper::startAdminArea($this);
     } else {
         AdminUIHelper::startAdminArea($this);
@@ -20,9 +19,6 @@
 
     // INCLUDE JS AND CSS
     $document->addStyleSheet(JUri::root().'media/com_zasilkovna/media/css/admin.css?v=' . filemtime(JPATH_ROOT . '/media/com_zasilkovna/media/css/admin.css'));
-    if ($isBelowJ4AndVM4) {
-        $document->addStyleSheet(JUri::root().'media/com_zasilkovna/media/css/vm-left-menu-j310.css?v=' . filemtime(JPATH_ROOT . '/media/com_zasilkovna/media/css/vm-left-menu-j310.css'));
-    }
 
     $document->addScript(JUri::root()."media/com_zasilkovna/media/js/repeater.js?v=" . filemtime(JPATH_ROOT . '/media/com_zasilkovna/media/js/repeater.js'));
     $document->addScript(JUri::root()."media/com_zasilkovna/media/js/admin.js?v=" . filemtime(JPATH_ROOT . '/media/com_zasilkovna/media/js/admin.js'));
@@ -34,7 +30,7 @@
         'config' => JText::_('PLG_VMSHIPMENT_PACKETERY_CONFIG_TAB'),
         'carriers' => JText::_('PLG_VMSHIPMENT_PACKETERY_CARRIERS_TAB'),
     ];
-    if (class_exists('vmuikitAdminUIHelper') && !$isBelowJ4AndVM4) {
+    if (class_exists('vmuikitAdminUIHelper')) {
         vmuikitAdminUIHelper::buildTabs($this, $tabs);
     } else {
         AdminUIHelper::buildTabs($this, $tabs);
@@ -49,7 +45,7 @@
     ?>
 </form>
 <?php
-if (class_exists('vmuikitAdminUIHelper')  && !$isBelowJ4AndVM4) {
+if (class_exists('vmuikitAdminUIHelper')) {
     vmuikitAdminUIHelper::endAdminArea();
 } else {
     AdminUIHelper::endAdminArea();
